9b7c567a2c9105a9ff46fc118a7772017e80538a
Samuel Neff <sam@serndesign.com>
2024-05-21 21:30:58 -0400

    Switch to commonjs module for compatibility

--- src/isEmpty.ts
+++ src/isEmpty.ts
@@ -2,7 +2,7 @@ import type { RecordKey } from './RecordKey';
 import { isNullOrUndefined } from './isNullOrUndefined';
 import { isString } from './isString';
 
-export function isEmpty(value: string | unknown[] | Record<RecordKey, unknown> | Map<unknown, unknown> | Set<unknown>) {
+export function isEmpty(value: undefined | null | string | unknown[] | Record<RecordKey, unknown> | Map<unknown, unknown> | Set<unknown>) {
   if (isNullOrUndefined(value)) {
     return true;
   }
