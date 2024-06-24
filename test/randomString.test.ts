9b0657c9caa5aff4e3814d1bbb9f48aeb6c04007
Samuel Neff <sam@serndesign.com>
2024-06-24 12:23:24 -0400

    Another fix to random*

--- test/randomString.test.ts
+++ test/randomString.test.ts
@@ -1,11 +1,11 @@
 import { defaultRandomChars, randomString } from '../src/randomString';
 
 test('5, undefined', () => {
-  const actual = randomString(5);
-  expect(actual.length).toBe(5);
-  const chars = actual.split('');
-  const invalidChars = chars.filter(
-    c => !(defaultRandomChars.includes(c))
-  );
-  expect(invalidChars).toEqual([]);
+  const validPattern = new RegExp("^" + `[${ defaultRandomChars }]`.repeat(5) + "$");
+
+  // not great but we're testing randomness so best we can do without mocking is running lots of iterations
+  for (let i = 0; i < 10000; i++) {
+    const actual = randomString(5);
+    expect(actual).toMatch(validPattern);
+  }
 });
