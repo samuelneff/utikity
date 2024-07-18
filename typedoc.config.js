fd3237c3bcc446351590e68bd5c3aad77af34fe6
Samuel Neff <sam@serndesign.com>
2024-07-18 19:08:13 -0400

    Docs styling

--- typedoc.config.js
+++ typedoc.config.js
@@ -4,10 +4,32 @@
 /** @type {import('typedoc').TypeDocOptions} */
 module.exports = {
   plugin: [
+    'typedoc-plugin-extras',
     'typedoc-plugin-mdn-links',
     'typedoc-plugin-replace-text',
   ],
+  basePath: './src',
+  customCss: './assets/docs.css',
   entryPoints: ["src/index.ts"],
+  excludeInternal: true,
+  favicon: 'assets/favicon.svg',
+  footerLastModified: true,
+  includeVersion: true,
+  intentionallyNotExported: [
+    'SpreadTwo',
+  ],
+  navigationLinks: {
+    "GitHub": "https://github.com/samuelneff/utikity",
+    "npm": "https://www.npmjs.com/package/utikity",
+  },
+  searchInComments: true,
+  searchInDocuments: true,
+  sort: 'alphabetical',
+  sourceLinkExternal: true,
+  validation: {
+    notDocumented: false, // Turn on when ready
+  },
+
   replaceText: {
     replacements: [
       {
